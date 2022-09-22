b13817e7a357c1f31e5591dcb257382245735a9c
M4TThys123 <70703948+M4TThys123@users.noreply.github.com>
2022-09-22 16:54:03 +0200

    🚑️ Critical hotfix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,6 +17,12 @@
       currentSlideItem = images.length - 1;
     }
   };
+
+  const consoleFunction = () => {
+    console.log(data.documents)
+  }
+
+  const currentImage = () => {};
 </script>
 
 <main>
@@ -44,32 +50,62 @@
           Dit zijn de <span class="text--purple">FDND Studenten</span>
         </h2>
         <ul class="laptop__list">
-          <li class="laptop remote remote-control">
-            <h2>FDND Studenten afstandsbediening</h2>
-            <div class="remote-control__buttons">
-              <button
-                class="btn btn--blue btn--left"
-                on:click={() => prevImage()}>Vorige</button
-              >
-              <button
-                class="btn btn--blue btn--right"
-                on:click={() => nextImage()}>Volgende</button
-              >
-            </div>
-            <img class="remote-control__logo" src="/philips-logo.svg" alt="" />
-          </li>
-          {#each data.documents as data}
           <li class="laptop">
-            <div class="laptop__img--wrapper">
-              <img src="./img/laptop-blue.svg" alt="" class="laptop__img" />
-              <img class="laptop__github-img" src="{data.data.githubimage.url}" alt="">
+            <div class="remote__container">
+              <h2 class="remote__title">FDND Studenten afstandsbediening</h2>
+              <div class="btn__container">
+                <button class="remote__btn" on:click={() => prevImage()}>
+                  <img src="/left-arrow.svg" alt="" class=""/>
+                  Vorige
+                </button>
+                <button class="remote__btn" on:click={() => nextImage()}>
+                  Volgende
+                  <img src="/right-arrow.svg" alt="" />
+                </button>
+              </div>
+              <img src="/philips-logo.svg" alt="" class="remote__img" />
             </div>
-              <h2>{data.data.name}</h2>
           </li>
+
+          <li class="laptop remote ">
+            <div class="remote-control">
+              <div class="max-width">
+                <h2 class="remote__title">FDND Studenten afstandsbediening</h2>
+                <div class="remote-control__buttons">
+                  <button
+                    class="btn btn--blue btn--left"
+                    on:click={() => prevImage()}>Vorige</button
+                  >
+                  <button
+                    class="btn btn--blue btn--right"
+                    on:click={() => nextImage()}>Volgende</button
+                  >
+                </div>
+              </div>
+              <img
+                class="remote-control__logo"
+                src="/philips-logo.svg"
+                alt=""
+              />
+            </div>
+          </li>
+
+          {#each data.documents as data}
+            <li class="laptop" on:click={() => consoleFunction(data.documents)}>
+              <div class="laptop__img--wrapper">
+                <img src="./img/laptop-blue.svg" alt="" class="laptop__img" />
+                <img
+                  class="laptop__github-img"
+                  src={data.data.githubimage.url}
+                  alt=""
+                />
+              </div>
+              <h2>{data.data.name}</h2>
+            </li>
           {/each}
         </ul>
       </div>
     </div>
   </section>
-  <!-- <pre>{JSON.stringify(data.documents, null, 2)}</pre> -->
+  <pre>{JSON.stringify(data.documents, null, 2)}</pre>
 </main>
